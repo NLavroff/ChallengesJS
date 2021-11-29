7c69c3e160eceff14f827f6d82c68682b71b8699
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-29 11:52:37 +0100

    Challenge8

---
+++ Challenge8.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+let tip;
+
+let calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300 ) {
+        tip = bill * (15 / 100);
+        return tip;
+    }
+    else {
+        tip = bill * (20 / 100);
+        return tip;
+    } 
+}
+
+tips = bills.map(calcTip);
+
+totals = bills.map((bill, index) => {
+    return bill + tips[index];
+});
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+
+let calcAverage = (array) => {
+    let sum = array.reduce((acc, curr) => acc + curr);
+    let average = sum / array.length;
+
+    return Math.round(average);
+}
+
+console.log(calcAverage(totals));
