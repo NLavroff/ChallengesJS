203636028ff5aa38dd1b4ae13a31bac9aa96fb10
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-30 11:34:51 +0100

    Challenge12

---
+++ Challenge12.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log("Données test 1 :");
+
+let temperatures1 = [17, 21, 23];
+
+for (let i = 0; i < temperatures1.length; i++) {
+    console.log(`${temperatures1[i]} degrés dans ${i + 1} jour(s).`);
+};
+
+console.log("Données test 2 :");
+
+let temperatures2 = [12, 5, -5, 0, 4];
+
+for (let i = 0; i < temperatures2.length; i++) {
+    console.log(`${temperatures2[i]} degrés dans ${i + 1} jour(s).`);
+};
